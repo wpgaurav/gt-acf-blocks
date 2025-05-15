42f860a0d25241fe1cb6d914d71b5b4b9ac98b63
Gaurav Tiwari <gaurav@gauravtiwari.org>
2025-05-15 10:02:00 +0530

    Improve post display.

--- blocks/post-display/post-display.php
+++ blocks/post-display/post-display.php
@@ -14,6 +14,8 @@ $show_date = get_field('pd_show_date') ?: false;
 $show_author = get_field('pd_show_author') ?: false;
 $title_tag = get_field('pd_title_tag') ?: 'h3';
 $custom_class = get_field('pd_custom_class') ?: '';
+$show_read_more = get_field('pd_show_read_more');
+$read_more_text = get_field('pd_read_more_text') ?: 'Read More';
 
 // Block unique ID
 $block_id = 'post-display-' . $block['id'];
@@ -58,22 +60,28 @@ $container_class = implode(' ', $container_classes);
                             <?php echo get_the_date('', $post->ID); ?>
                         </span>
                     <?php endif; ?>
+
+                    <?php if ($show_read_more): ?>
+                        <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="post-read-more-button text-link-read-more">
+                            <?php echo esc_html($read_more_text); ?>
+                        </a>
+                    <?php endif; ?>
                 </li>
             <?php endforeach; ?>
         </ul>
         
     <?php elseif ($layout === 'thumbnail'): ?>
     
-        <div class="post-display-thumbnail">
+        <div class="post-display-thumbnail-layout">
             <?php foreach ($selected_posts as $post): ?>
                 <div class="post-item">
+                    <?php if (has_post_thumbnail($post->ID)): ?>
                     <div class="post-thumbnail">
-                        <?php if (has_post_thumbnail($post->ID)): ?>
-                            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>">
-                                <?php echo get_the_post_thumbnail($post->ID, 'thumbnail', ['class' => 'post-thumb']); ?>
-                            </a>
-                        <?php endif; ?>
+                        <a href="<?php echo esc_url(get_permalink($post->ID)); ?>">
+                            <?php echo get_the_post_thumbnail($post->ID, 'thumbnail', ['class' => 'post-thumb']); ?>
+                        </a>
                     </div>
+                    <?php endif; ?>
                     
                     <div class="post-content">
                         <<?php echo esc_attr($title_tag); ?> class="post-title">
@@ -92,7 +100,7 @@ $container_class = implode(' ', $container_classes);
                                 
                                 <?php if ($show_author): ?>
                                     <span class="post-author">
-                                        by <?php echo get_the_author_meta('display_name', $post->post_author); ?>
+                                        by <?php echo esc_html(get_the_author_meta('display_name', $post->post_author)); ?>
                                     </span>
                                 <?php endif; ?>
                             </div>
@@ -100,8 +108,14 @@ $container_class = implode(' ', $container_classes);
                         
                         <?php if ($show_excerpt): ?>
                             <div class="post-excerpt">
-                                <?php echo get_the_excerpt($post->ID); ?>
+                                <?php echo wp_kses_post(get_the_excerpt($post->ID)); ?>
                             </div>
+                        <?php endif; ?>
+
+                        <?php if ($show_read_more): ?>
+                            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="post-read-more-button">
+                                <?php echo esc_html($read_more_text); ?>
+                            </a>
                         <?php endif; ?>
                     </div>
                 </div>
@@ -110,9 +124,9 @@ $container_class = implode(' ', $container_classes);
         
     <?php elseif ($layout === 'grid'): ?>
     
-        <div class="post-display-grid">
+        <div class="post-display-grid-layout">
             <?php foreach ($selected_posts as $post): ?>
-                <div class="grid-item">
+                <article class="grid-item post-item">
                     <?php if (has_post_thumbnail($post->ID)): ?>
                         <div class="post-thumbnail">
                             <a href="<?php echo esc_url(get_permalink($post->ID)); ?>">
@@ -121,34 +135,42 @@ $container_class = implode(' ', $container_classes);
                         </div>
                     <?php endif; ?>
                     
-                    <<?php echo esc_attr($title_tag); ?> class="post-title">
-                        <a href="<?php echo esc_url(get_permalink($post->ID)); ?>">
-                            <?php echo esc_html(get_the_title($post->ID)); ?>
-                        </a>
-                    </<?php echo esc_attr($title_tag); ?>>
-                    
-                    <?php if ($show_date || $show_author): ?>
-                        <div class="post-meta">
-                            <?php if ($show_date): ?>
-                                <span class="post-date">
-                                    <?php echo get_the_date('', $post->ID); ?>
-                                </span>
-                            <?php endif; ?>
-                            
-                            <?php if ($show_author): ?>
-                                <span class="post-author">
-                                    by <?php echo get_the_author_meta('display_name', $post->post_author); ?>
-                                </span>
-                            <?php endif; ?>
-                        </div>
-                    <?php endif; ?>
-                    
-                    <?php if ($show_excerpt): ?>
-                        <div class="post-excerpt">
-                            <?php echo get_the_excerpt($post->ID); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
+                    <div class="post-content">
+                        <<?php echo esc_attr($title_tag); ?> class="post-title">
+                            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>">
+                                <?php echo esc_html(get_the_title($post->ID)); ?>
+                            </a>
+                        </<?php echo esc_attr($title_tag); ?>>
+                        
+                        <?php if ($show_date || $show_author): ?>
+                            <div class="post-meta">
+                                <?php if ($show_date): ?>
+                                    <span class="post-date">
+                                        <?php echo get_the_date('', $post->ID); ?>
+                                    </span>
+                                <?php endif; ?>
+                                
+                                <?php if ($show_author): ?>
+                                    <span class="post-author">
+                                        by <?php echo esc_html(get_the_author_meta('display_name', $post->post_author)); ?>
+                                    </span>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                        
+                        <?php if ($show_excerpt): ?>
+                            <div class="post-excerpt">
+                                <?php echo wp_kses_post(get_the_excerpt($post->ID)); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if ($show_read_more): ?>
+                            <a href="<?php echo esc_url(get_permalink($post->ID)); ?>" class="post-read-more-button">
+                                <?php echo esc_html($read_more_text); ?>
+                            </a>
+                        <?php endif; ?>
+                    </div>
+                </article>
             <?php endforeach; ?>
         </div>
         
